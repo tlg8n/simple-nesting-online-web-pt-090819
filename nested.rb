30d6ad26f7f55ccff618f7efeb6e344601355c2b
John Schmidley <tlg8n@outlook.com>
2019-10-01 08:12:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,8 +37,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
-programmer_hash [:alan_kay]
+
+
 def dennis_ritchies_language
 	programmer_hash = 
  		{
