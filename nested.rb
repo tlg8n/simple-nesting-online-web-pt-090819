9b0c690064ada32ef54a42572b9eede8f5b0d3c3
John Schmidley <tlg8n@outlook.com>
2019-10-01 08:39:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,10 +83,13 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] = {
+   programmer_hash[:yukihiro_matsumoto] = {
     known_for: "Ruby",
-    languages:["LISP", "C"]
+    languages:
+      ["LISP", "C"]
     }
+    
+  programmer_hash
 end
 
 def changing_alan
